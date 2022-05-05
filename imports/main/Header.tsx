0a568d7894c6c748969dbc32be501ff760ad45e1
Matt Black <mb@mattblack.io>
2022-05-05 10:09:30 -0400

    Stay flexible

--- imports/main/Header.tsx
+++ imports/main/Header.tsx
@@ -5,12 +5,12 @@ import { useUsername } from '/imports/users/useUser'
 
 export function Header() {
   return (
-    <header className="px-7 pt-7 pb-7 md:mb-16 flex flex-wrap">
+    <header className="px-7 pt-7 pb-7 md:mb-16 flex">
       <h1
         className={ctl(`
           flex
           flex-1
-          text-2xl xs:4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl
+          text-2xl xs:3xl sm:text-4xl md:text-7xl
           font-extrabold 
           [text-shadow:0_7px_4px_rgba(1,0,1,0.74)]
           tracking-wider
@@ -20,7 +20,6 @@ export function Header() {
         <Link
           to="/"
           className={ctl(`
-            whitespace-nowrap
             hover:underline
           `)}
         >
@@ -41,32 +40,29 @@ function UsernameOrLogin() {
   const username = useUsername()
   const showLogin = !username && pathname !== '/login'
   const showProfile = !!username
+  const classNames = ctl(`
+    self-center
+    text-right
+    flex-1
+    text-xl xs:text-2xl sm:text-3xl
+    transition-all 
+    text-stone-700 hover:text-stone-400
+    active:text-stone-100
+    font-bold
+    cursor-pointer
+    hover:underline
+    whitespace-nowrap
+  `)
 
   if (showLogin) {
     return (
-      <Link to="/login" className="hover:underline">
+      <Link to="/login" className={classNames}>
         Login
       </Link>
     )
   } else if (showProfile) {
     return (
-      <Link
-        to="/profile"
-        className={ctl(`
-          self-center
-          text-right
-          flex-1
-          md:ml-7
-          text-xl xs:text-2xl sm:text-3xl
-          transition-all 
-          text-stone-700 hover:text-stone-400
-          active:text-stone-100
-          font-bold
-          cursor-pointer
-          hover:underline
-          whitespace-nowrap
-        `)}
-      >
+      <Link to="/profile" className={classNames}>
         {username}
       </Link>
     )
