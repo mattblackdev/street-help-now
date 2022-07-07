195349ff4fd7e2e7b3655b9bd11cbaa8856e7f6c
Matt Black <mb@mattblack.io>
2022-07-06 22:14:45 -0400

    Invited on the cloud

--- server/sendInvitationEmail.js
+++ server/sendInvitationEmail.js
@@ -2,16 +2,19 @@ import { Email } from 'meteor/email'
 import { UserInvites } from '/imports/users/api/collection'
 
 export function sendInvitationEmail({ email, userInvitesId }) {
+  const rootUrl = process.env.ROOT_URL.replace(/\/$|$/, '/')
+  const joinLink = `${rootUrl}invites/${userInvitesId}`
+
   Email.send({
     from: 'invites@streethelpnow.com',
     to: email,
     subject: 'Street Help Now - Invitation to Join',
-    html: `<div>
-      <title><h1>You've been invited to Street Help Now</h1></title>
-      <a href="${process.env.ROOT_URL}invites/${userInvitesId}">Join Now</a>
+    html: `<html><body><div>
+      <title><h1>You've been invited to Street Help Now.</h1></title>
+      <p><a href="${joinLink}">Click here to Join.</a></p>
       <footer>
         <p>Thanks,<br/>Street Help Now Team</p>
       </footer>
-    </div>`,
+    </div></body></html>`,
   })
 }
